d3342043f59b17969fe0c3840a71c76e41855778
Maurus Calzaferri <Maurus Calzaferri@192.168.1.112>
2018-10-20 13:50:59 +0200

    Bugfix: Nullpointer in GeoLocationAttribute when calling decode

--- Common/src/main/mcalzaferri/net/attributes/GeoLocationAttribute.java
+++ Common/src/main/mcalzaferri/net/attributes/GeoLocationAttribute.java
@@ -26,9 +26,9 @@ public class GeoLocationAttribute extends Attribute {
     @Override
     public void decode(String data) {
         String[] values = data.split(",");
+
         if(values.length == 2){
-            value.latitude = Double.parseDouble(values[0]);
-            value.longitude = Double.parseDouble(values[1]);
+            value = new GeoLocation(Double.parseDouble(values[0]), Double.parseDouble(values[1]));
         }else{
             throw new IllegalArgumentException("Argument is not of type (latitude,longitude)");
         }
