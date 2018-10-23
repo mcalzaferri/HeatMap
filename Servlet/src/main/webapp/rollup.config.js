680bc58b3248066bd3cc3924634b4a895e5486e4
Maurus Calzaferri <Maurus Calzaferri@192.168.1.112>
2018-10-23 23:58:01 +0200

    Added npm and rollup files

---
+++ Servlet/src/main/webapp/rollup.config.js
@@ -0,0 +1,17 @@
+import cjs from 'rollup-plugin-commonjs';
+import node from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'map.js',
+  output: [
+    {file: 'bundle.js', format: 'iife'}
+  ],
+  plugins: [
+    node(),
+    cjs(),
+    production && terser()
+  ]
+};
