e01f22fae805e5f4ccb0e8eb2867aa9bb3004f18
mcalzaferri <mcalzaferri@pcinf050.ntb.ch>
2018-11-15 17:36:33 +0100

    Added charts

--- Servlet/src/main/webapp/leaflet.js
+++ Servlet/src/main/webapp/leaflet.js
@@ -1,32 +1,19 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 3);
 
-window.onload = function () {
-    $.get("https://heatmap-219120.appspot.com/api/sensors", function(data, status){
-        for(var i = 0; i < data.length; i++){
-            var marker = L.marker([data[i].location.latitude, data[i].location.longitude]).addTo(mymap);
-            marker.bindPopup();
-            marker.id = data[i].id;
-            marker.on('click', onMarkerClick );
-            function onMarkerClick(e) {
-                var popup = e.target.getPopup();
-                popup.setContent("" +  e.target.id );
-            }
-        }
-    });
-}
+window.onload = addMarkersToMap();
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 9,
     minZoom: 1,
     zoomDelta: 0.2,
-    noWrap: true,
+    //noWrap: true,
     bounds: [
-        [-90, -180],
-        [90, 180]
+        [-90, -210],
+        [90, 210]
       ],
     maxBounds: [
-        [-90, -180],
-        [90, 180]
+        [-90, -210],
+        [90, 210]
       ],
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -74,7 +61,58 @@ function calculateMinZoom(newSize){
 
 function panToCenter(){
     mymap.panInsideBounds([
-        [-90, -180],
-        [90, 180]
+        [-90, -210],
+        [90, 210]
       ], animate = true);
+}
+
+function addMarkersToMap(){
+    $.get("https://heatmap-219120.appspot.com/api/sensors", function(data, status){
+        for(var i = 0; i < data.length; i++){
+            var marker = L.marker([data[i].location.latitude, data[i].location.longitude]).addTo(mymap);
+            marker.bindPopup();
+            marker.id = data[i].id;
+            marker.on('click', onMarkerClick );
+            function onMarkerClick(e) {
+                var popup = e.target.getPopup();
+                var chartId = "chart" + e.target.id;
+                popup.setContent(
+                    '<canvas id="' + chartId + '" width="400" height="400"><p>ERROR: Could not load data!</p></canvas>'
+                );
+                drawChart(document.getElementById(chartId), e.target.id);
+            }
+        }
+    });
+}
+
+function drawChart(ctx, sensorId){
+    var url = "https://heatmap-219120.appspot.com/api/sensors/" + sensorId + "/measurements?orderBy=+timestamp";
+    var measurements = [];
+    $.get(url, function(data, status){
+        for(var i = 0; i < data.length; i++){
+            var point = {
+                x : new Date(data[i].timestamp),
+                y : data[i].temperature
+            };
+            
+            measurements.push(point);
+        }
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                datasets: [{
+                    label:"Temperature",
+                    data: measurements
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        position: 'bottom'
+                    }]
+                }
+            }
+        });
+    });
 }
